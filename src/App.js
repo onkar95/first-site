8e67ca352be76511c514aa929dad02a3e7f6ad75
onkar95 <66904041+onkar95@users.noreply.github.com>
2020-12-30 20:57:08 +0530

    f1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import Navbar from './Navbar';
+import Home from './Home';
+// import Contact from './Contact';
+// import Blog from './Blogs'
+// import Boxes from './Boxes'
+import './blog.css'
+import './contactAndFooter.css'
+import './navbar.css'
+import './blog-menu.css'
+import './index.css'
+import './navbarbuttons.css'
+import { Route, Switch } from 'react-router-dom';
+// import Footer from './Footer';
+// import Showing from './Showing';
+// import Extrabox from './Extrabox';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < >
+      {/* <Navbar /> */}
+      <Home/>
+      <Switch>
+        <Route path='/home' component={Home}></Route>
+      </Switch>
+      {/* <Switch>
+        <Route path='/blog' component={Blog}></Route>
+      </Switch>
+      <br/>
+      <Switch>
+        <Route path='/contact' component={Contact}></Route>
+      </Switch>
+      <Switch>
+        <Route path='/blog-boxes' component={Extrabox}></Route>
+      </Switch>
+      <hr/>
+      <Footer/> */}
+    </>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+// import Boxes from './Boxes';
+// import image from './images/img8.jpg'
+// import Arrowl from '@material-ui/icons/ArrowBackIos';
+// import Arrowr from '@material-ui/icons/ArrowForwardIos';
+
+function Home() {
+    return (
+        <>
+            <h1>hello</h1>
+            <div>by</div>
+        </>
+    )
+}
+
+export default Home;
+
